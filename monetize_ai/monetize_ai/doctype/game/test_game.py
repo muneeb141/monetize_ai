a69b65f51c5a5dba06a21f6ca20afdf3ecb62bb6
muneeb141 <muneebmohammed141@gmail.com>
2023-03-31 12:14:56 +0000

    feat: create gaming monitor

---
+++ monetize_ai/monetize_ai/doctype/game/test_game.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Muneeb Mohammed and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGame(FrappeTestCase):
+	pass
---
+++ monetize_ai/monetize_ai/doctype/mai_leaderboard/mai_leaderboard.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Muneeb Mohammed and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('MAI Leaderboard', {
+	// refresh: function(frm) {
+
+	// }
+});
